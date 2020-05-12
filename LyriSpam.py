2db75001172fd12bd2176d5cbb476221d42f85b7
Nukala Suraj <nukalasuraj7@gmail.com>
2020-05-12 13:32:27 +0530

    Multi User Support and File Based Approach

--- LyriSpam.py
+++ LyriSpam.py
@@ -1,89 +1,43 @@
 """
-This Code Is Written by NukeSuraj
+author: @NukeSuraj
 """
 
-import pyautogui as pg
+import pyautogui as pg 
+# to install: run cmd, cd C:\Users\nukal\AppData\Local\Programs\Python\Python38-32\Scripts, pip install pyautogui
 import time    
 
-# Name Of The Person to Be .....
-uname = ""      
+# Names(as saved in whatsapp) Of The People to Be .....
+uname = ["Karthik","Raki","Varun"]    
 
 # The Song Lyrics
-song = """Light 'em up, light 'em up
-Tell me where you are, tell me where you are
-Summer nights, bright lights
-And the shootin' stars, they break my heart
-Callin' you now, but you're not pickin' up
-Shadows so close if that's still enough
-Light a match, light a match
-Baby, in the dark, show me where you are
-Oh, love
-How I miss you every single day
-When I see you on those streets
-Oh, love
-Tell me there's a river I can swim that will bring you back to me
-'Cause I don't know how to love someone else
-I don't know how to forget your face
-No, love
-God, I miss you every single day and now you're so far away
-So far away
-It's breakin' me, losin' you
-We were far from perfect
-But we were worth it
-Too many fights, and we cried
-But never said we're sorry
-Stop sayin' you love me
-You're callin' me now, but I can't pick up
-Your shadow's still close, and I'm still in love
-The summer's over now
-But somehow it still breaks my heart
-We could have had this talk
-Oh
-Oh, love
-How I miss you every single day
-When I see you on those streets
-Oh, love
-Tell me there's a river I can swim that will bring you back to me
-'Cause I don't know how to love someone else
-I don't know how to forget your face
-No, love
-God, I miss you every single day and now you're so far away
-So far away
-So far away, oh
-So far away
-So far away
-Oh, love
-How I miss you every single day
-When I see you on those streets
-Oh, love
-Tell me there's a river I can swim that will bring you back to me
-'Cause I don't know how to love someone else
-I don't know how to forget your face
-Oh, love
-God, I miss you every single day when you're so far away"""
+sname = input("Enter Lyrics File:") #RapGod.txt/SoFarAway.txt for now or you can create your own text file
+
+with open(sname,"r") as f:
+    song = f.readlines()
 
 t = 0.5
 
 wa = "https://web.whatsapp.com/"
-ig = "https://www.instagram.com/"
 
 def urlhandler(url):
-    pg.click(x=235,y=76)
+    pg.click(x=235,y=76) 
     time.sleep(t)
     pg.typewrite(url)
     time.sleep(t)
     pg.hotkey('enter')
     time.sleep(10)
-    pg.click(x=116,y=228)
-    time.sleep(t)
-    pg.typewrite(uname)
-    time.sleep(t)
-    pg.click(x=135,y=412)
-    time.sleep(t)
-    pg.click(x=945,y=913)
-    time.sleep(t)
-    pg.typewrite(song)
-    pg.hotkey('enter')
+    for name in uname:
+        pg.click(x=116,y=228)
+        time.sleep(t)
+        pg.typewrite(name)
+        time.sleep(t)
+        pg.click(x=135,y=412)
+        time.sleep(t)
+        pg.click(x=804,y=974)
+        for line in song:
+            pg.typewrite(line)
+            pg.hotkey('enter')
+            #time.sleep(t)
 
 pg.hotkey('win','2')
 time.sleep(1)
